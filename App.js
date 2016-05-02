6e9ae5060a2589570077831986f03522ef0f67e4
Kndarp Patel <kndarppatel@gmail.com>
2016-05-03 00:25:08 +0530

    Dev Environment Setup

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+});
+
+export default App;
